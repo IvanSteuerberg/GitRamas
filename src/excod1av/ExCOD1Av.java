c4dec8cf9293f4041ff119b8e2860b5067f29000
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-01-22 13:55:53 +0100

    E

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -7,6 +7,7 @@ package excod1av;
 public class ExCOD1Av {
     
     public static void main(String[] args) {
+        System.out.println("Si quiere grano autor, le dejo mi tractor");
         System.out.println("Fin de la cita");
     }
     
